7a9d2af88320ec6a35a6bbbaed81ff651e4d1558
Govindan007 <sgovindan077@gmail.com>
2025-04-28 05:00:01 +0530

    disk sheduling

---
+++ disk sheduling/c_scan.c
@@ -0,0 +1,50 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+int main() { 
+    int n, head, max_cylinder, seek_time = 0, i, prev, index = 0; 
+    printf("Enter total number of disk requests: "); 
+    scanf("%d", &n); 
+    int requests[n + 2];  
+    printf("Enter disk requests: "); 
+    for (i = 0; i < n; i++) { 
+        scanf("%d", &requests[i]); 
+    } 
+    printf("Enter initial head position: "); 
+    scanf("%d", &head); 
+    printf("Enter maximum cylinder number: "); 
+    scanf("%d", &max_cylinder); 
+    requests[n] = 0;  
+    requests[n + 1] = max_cylinder; 
+    n += 2; 
+    for (i = 0; i < n - 1; i++) { 
+        for (int j = i + 1; j < n; j++) { 
+            if (requests[i] > requests[j]) { 
+                int temp = requests[i]; 
+                requests[i] = requests[j]; 
+                requests[j] = temp; 
+            } 
+        } 
+    } 
+    while (index < n && requests[index] < head) { 
+        index++; 
+    } 
+    prev = head; 
+    printf("\nC-SCAN Order: "); 
+ 
+ 
+for (i = index; i < n; i++) { 
+printf("%d -> ", requests[i]); 
+seek_time += abs(requests[i] - prev); 
+prev = requests[i]; 
+} 
+printf("0 -> "); 
+seek_time += abs(prev - 0); 
+prev = 0; 
+for (i = 0; i < index; i++) { 
+printf("%d -> ", requests[i]); 
+seek_time += abs(requests[i] - prev); 
+prev = requests[i]; 
+} 
+printf("End\nTotal Seek Time: %d\n", seek_time); 
+return 0; 
+} 
